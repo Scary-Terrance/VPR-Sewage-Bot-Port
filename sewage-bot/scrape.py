b42098b3d65485fe11e5da51dff4cd9f8104654c
Scary-Terrance <bnv12310@ccv.vsc.edu>
2018-07-20 20:34:38 -0400

    Ported the bot to python3, added some error handling

--- sewage-bot/scrape.py
+++ sewage-bot/scrape.py
@@ -2,67 +2,82 @@
 
 import csv
 import requests
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 
 url = "https://anrweb.vt.gov/DEC/WWInventory/SewageOverflows.aspx"
-response = requests.get(url)
-html = response.content
-soup = BeautifulSoup(html)
 
-table1 = soup.find("table", attrs={"class": "dataList", "id": "body_GridViewPublicAlerts"})
-table2 = soup.find("table", attrs={"class": "dataList", "id": "body_GridViewSewageOverflowsUnOfficial"})
-table3 = soup.find("table", attrs={"class": "dataList", "id": "body_GridViewSewageOverflowsAuthorized"})
-table4 = soup.find("table", attrs={"class": "dataList", "id": "body_GridViewSewageOverflowsOther"})
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0'
+}
 
-list_of_rows1 = []
-list_of_rows2 = []
-list_of_rows3 = []
-list_of_rows4 = []
-final_list = []
+try:
+    response = requests.get(url, headers=headers, timeout=5)
+    html = response.content
+    soup = BeautifulSoup(html, "lxml")
 
-for row in table1.findAll("tr"):
-    list_of_cells = []
-    list_of_cells.append("Public Alert")
-    for cell in row.findAll("td"):
-        text = cell.text.replace("&nbsp;", "")
-        list_of_cells.append(text)
-    list_of_rows1.append(list_of_cells)
-del list_of_rows1[-1]
+    try:
+        table1 = soup.find("table", attrs={"class": "dataList", "id": "body_GridViewPublicAlerts"})
+        table2 = soup.find("table", attrs={"class": "dataList", "id": "body_GridViewSewageOverflowsUnOfficial"})
+        table3 = soup.find("table", attrs={"class": "dataList", "id": "body_GridViewSewageOverflowsAuthorized"})
+        table4 = soup.find("table", attrs={"class": "dataList", "id": "body_GridViewSewageOverflowsOther"})
 
-for row in table2.findAll("tr"):
-    list_of_cells = []
-    list_of_cells.append("Pending Review")
-    for cell in row.findAll("td"):
-        text = cell.text.replace("&nbsp;", "")
-        list_of_cells.append(text)
-    list_of_rows2.append(list_of_cells)
-del list_of_rows2[-1]
+        list_of_rows1 = []
+        list_of_rows2 = []
+        list_of_rows3 = []
+        list_of_rows4 = []
+        final_list = []
 
-for row in table3.findAll("tr"):
-    list_of_cells = []
-    list_of_cells.append("Reviewed")
-    for cell in row.findAll("td"):
-        text = cell.text.replace("&nbsp;", "")
-        list_of_cells.append(text)
-    list_of_rows3.append(list_of_cells)
-del list_of_rows3[-1]
+        for row in table1.findAll("tr"):
+            list_of_cells = []
+            list_of_cells.append("Public Alert")
+            for cell in row.findAll("td"):
+                text = cell.text.replace("&nbsp;", "")
+                list_of_cells.append(text)
+            list_of_rows1.append(list_of_cells)
+        del list_of_rows1[-1]
 
-for row in table4.findAll("tr"):
-    list_of_cells = []
-    list_of_cells.append("Reviewed")
-    for cell in row.findAll("td"):
-        text = cell.text.replace("&nbsp;", "")
-        list_of_cells.append(text)
-    list_of_rows4.append(list_of_cells)
-del list_of_rows4[-1]
+        for row in table2.findAll("tr"):
+            list_of_cells = []
+            list_of_cells.append("Pending Review")
+            for cell in row.findAll("td"):
+                text = cell.text.replace("&nbsp;", "")
+                list_of_cells.append(text)
+            list_of_rows2.append(list_of_cells)
+        del list_of_rows2[-1]
 
-final_list = list_of_rows1 + list_of_rows2 + list_of_rows3 + list_of_rows4
+        for row in table3.findAll("tr"):
+            list_of_cells = []
+            list_of_cells.append("Reviewed")
+            for cell in row.findAll("td"):
+                text = cell.text.replace("&nbsp;", "")
+                list_of_cells.append(text)
+            list_of_rows3.append(list_of_cells)
+        del list_of_rows3[-1]
 
-while [] in final_list:
-    final_list.remove([])
-    final_list.sort()
+        for row in table4.findAll("tr"):
+            list_of_cells = []
+            list_of_cells.append("Reviewed")
+            for cell in row.findAll("td"):
+                text = cell.text.replace("&nbsp;", "")
+                list_of_cells.append(text)
+            list_of_rows4.append(list_of_cells)
+        del list_of_rows4[-1]
 
-outfile = open("/home/vprnet/webapps/bots/bots/sewage-bot/sewage.csv", "wb")
-writer = csv.writer(outfile)
-writer.writerow(["Status", "Index", "Start Date", "End Date", "Start/End Times", "Municipality", "Location", "Waterbody", "Description of Incident", "Estimated Volume (gallons)", "Wastewater Treatment Facility", "Contact Person"])
-list = writer.writerows(final_list)
+        final_list = list_of_rows1 + list_of_rows2 + list_of_rows3 + list_of_rows4
+
+        while [] in final_list:
+            final_list.remove([])
+            final_list.sort()
+
+        outfile = open("data/sewage.csv", "w")
+        writer = csv.writer(outfile)
+        writer.writerow(["Status", "Index", "Start Date", "End Date", "Start/End Times", "Municipality", "Location", "Waterbody", "Description of Incident", "Estimated Volume (gallons)", "Wastewater Treatment Facility", "Contact Person"])
+        list = writer.writerows(final_list)
+
+        print("Scraping Complete")
+
+    except AttributeError as e:
+        print("Error accessing State Database: ", str(e))
+
+except requests.exceptions.RequestException as e:
+    print("Error loading web page: ", str(e))
--- sewage-bot/tweet.py
+++ sewage-bot/tweet.py
@@ -1,14 +1,14 @@
 #!/usr/local/bin/python2.7
 
 from twython import Twython
-from config import consumer_key, consumer_secret, access_token, access_token_secret
+#from config import consumer_key, consumer_secret, access_token, access_token_secret
 from index import new_tweet
 import logging
 
-twitter = Twython(consumer_key, consumer_secret, access_token, access_token_secret)
+#twitter = Twython(consumer_key, consumer_secret, access_token, access_token_secret)
 
 try:
-    with open("/home/vprnet/webapps/bots/bots/sewage-bot/past_tweets.log", "r") as f:
+    with open("data/past_tweets.log", "r") as f:
         reader = f.readlines()
         past_tweets = []
 
@@ -22,11 +22,11 @@ try:
             duplicate = [s for s in past_tweets if sewage_tweet in s]
 
             if duplicate:
-                print "Already been tweeted."
+                print ("Already been tweeted.")
             else:
-                logging.basicConfig(filename="/home/vprnet/webapps/bots/bots/sewage-bot/past_tweets.log", level=logging.INFO)
+                logging.basicConfig(filename="data/past_tweets.log", level=logging.INFO)
                 logging.info(sewage_tweet)
-                twitter.update_status(status=sewage_tweet)
-                print "New tweet tweeted."
+                #twitter.update_status(status=sewage_tweet)
+                print ("New tweet tweeted.")
 except TypeError:
-    print "No new sewage spills reported."
+    print ("No new sewage spills reported.")
